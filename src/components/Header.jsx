3606b6180e669fa46bba733c45df82db5733b575
LeightonSkervin <lskervin23@gmail.com>
2024-03-04 04:20:11 -0500

    css update, game cards rendered

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import GenreFilter from './GenreFilter'
 import Search from './Search'
+import Logo from './Logo'
 
 function Header() {
   return (
-    <nav className="menu">
-    <div className='header'>Header</div>
-  <button>
-    <span className="profile-logo" role="img" aria-label="user">
-      👤
-    </span>
-    <span className="profile-menu">username</span>
-  </button>
+<nav className="menu">
+  <div className='header'><Logo/></div>
+  <div className="user-container">
+    <div className="button-container">
+      <button>
+        <span className="profile-logo" role="img" aria-label="user">
+          👤
+        </span>
+        <span className="profile-menu">username</span>
+      </button>
+    </div>
+  </div>
 </nav>
 );
 }
